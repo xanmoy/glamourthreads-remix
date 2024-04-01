b100c00c55ac9b8ef74566ca106fa55d2979c696
Tanmoy Ganguly <contact@xanmoy.me>
2024-04-02 03:38:58 +0530

    added Footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      
+        <footer
+            className="flex flex-col items-center bg-white text-center text-surface">
+           
+                
+                
+
+            
+          <div className="w-full bg-white p-4 text-center">
+              <span>
+              © 2024 
+              <a href="https://store.xanmoy.me/"> Glamour Threads</a>
+                  . All rights reserved.
+              </span>
+            </div>
+        </footer>
+  )
+}
+
+export default Footer
